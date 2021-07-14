1ad592c2ed5023b6035c8b7a29327c55f4f83c10
Stefania9797 <stefaniamastrel@gmail.com>
2021-07-14 17:03:48 +0200

    add table head

--- comics/resources/views/comics/index.blade.php
+++ comics/resources/views/comics/index.blade.php
@@ -3,4 +3,29 @@
 @section('content')
 <h2>Comics List</h2>
     <a href="{{ route('create') }}">Add Comic</a>
+    <table>
+        <thead>
+            <tr>
+                <th>Title</th>
+                <th>Description</th>
+                <th>Thumb</th>
+                <th>Price</th>
+                <th>Series</th>
+                <th>Sale date</th>
+                <th>Type</th>
+            </tr>
+        </thead>
+
+        <!-- <tbody>
+            @foreach($comics as $comic)
+                <tr>
+                    <td>{{ $comic->title }}</td>
+                    <td>{{ $comic->series }}</td>
+                    <td>{{ $comic->description }}</td>
+                    <td>{{ $comic->cover}}</td>
+                </tr>
+
+            @endforeach
+        </tbody> -->
+    </table>
 @endsection
